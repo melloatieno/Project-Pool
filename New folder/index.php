3030ed9b459e8db2a0488b249cf2d74e032e6ace
melloatieno <atienomelvine98@gmail.com>
2020-10-14 08:14:35 +0300

    Add files via upload

---
+++ New folder/index.php
@@ -0,0 +1,16 @@
+<?php
+include('connect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti voluptas porro reiciendis hic earum. Similique corporis quis veritatis voluptatem esse. Minus, inventore aut? Consequatur totam saepe atque soluta quasi?</p>
+</body>
+</html>
+  
